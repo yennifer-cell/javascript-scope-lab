294a3b3dca5c83aa6a1b39c526b1901cf83ee1f4
yennifer <shiksjayne95@gmail.com>
2026-01-04 13:25:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// Global scope
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = ['Strawberry', 'Milkshake'];
+
+//function and block 
+function addBurger() {
+  let newBurger = 'Flatburger';
+   burgers.push(newBurger);
+}
+
+  //variable declaration and scope
+  if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+     burgers.push(anotherNewBurger);
+  }
+
+    // add another function
+    function changeFeaturedDrink() {
+      let FeaturedDrink = 'The Javashake'
+    }
+
+//call the function to test 
+addBurger();
+console.log('After adding burger:', burgers);
+
+changeFeaturedDrink();
+console.log('After changing drink:', featuredDrink);
